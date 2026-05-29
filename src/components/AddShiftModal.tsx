4ebd327a13ad1419b1c3fa90cb74efa4999d09c2
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2026-05-29 09:52:55 +0400

    Updated AddShiftModal.tsx

--- src/components/AddShiftModal.tsx
+++ src/components/AddShiftModal.tsx
@@ -424,7 +424,7 @@ export const AddShiftModal: React.FC<AddShiftModalProps> = ({
             </div>
             
             <div className="grid grid-cols-2 gap-4">
-              <div>
+              <div className="flex flex-col items-center">
                 <label className="block text-sm font-medium text-gray-700 mb-2 text-center">From</label>
                 <input
                   type="time"
@@ -436,7 +436,7 @@ export const AddShiftModal: React.FC<AddShiftModalProps> = ({
                   className="w-full px-1 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-center text-sm"
                 />
               </div>
-              <div>
+              <div className="flex flex-col items-center">
                 <label className="block text-sm font-medium text-gray-700 mb-2 text-center">To</label>
                 <input
                   type="time"
